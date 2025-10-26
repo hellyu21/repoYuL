78cd2e87c4f73d3079f242a865ccc1d654312e5f
ElenaYurova21 <59369268+hellyu21@users.noreply.github.com>
2025-10-26 22:40:18 +0200

    upd form.html, fix index.php

--- lab5/www/index.php
+++ lab5/www/index.php
@@ -8,7 +8,7 @@ $all = $pet->getAll();
 <h2>Сохранённые данные:</h2>
 <ul>
 <?php foreach($all as $row): ?>
-    <li><?= $row['owner_name'] ?>, <?= $row['pet_age'] ?> лет, <?= $row['pet_type'] ?>, Вакцинация: <?= $row['has_vaccinations'] ? 'Да' : 'Нет' ?>, Пол: <?= $row['pet_gender ']?></li>
+    <li><?= $row['owner_name'] ?>, <?= $row['pet_age'] ?> лет, <?= $row['pet_type'] ?>, Вакцинация: <?= $row['has_vaccinations'] ? 'Да' : 'Нет' ?>, Пол: <?= $row['pet_gender']?></li>
 <?php endforeach; ?>
 </ul>
 
