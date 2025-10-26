632aa9e1565a3df9a56ea63354143a964db2115f
ElenaYurova21 <59369268+hellyu21@users.noreply.github.com>
2025-10-26 22:11:19 +0200

    fix docker compose, upd index.php

--- lab5/www/index.php
+++ lab5/www/index.php
@@ -1,36 +1,15 @@
-<?php session_start(); ?>
-<?php require_once 'UserInfo.php'; ?>
+<?php
+require 'db.php';
+require 'Pet.php';
 
-<?php $info = UserInfo::getInfo(); ?>
-<h3>Информация о пользователе:</h3>
-<?php foreach ($info as $key => $val): ?>
-    <?= htmlspecialchars($key) ?> : <?= htmlspecialchars($val) ?><br>
+$pet = new Pet($pdo);
+$all = $pet->getAll();
+?>
+<h2>Сохранённые данные:</h2>
+<ul>
+<?php foreach($all as $row): ?>
+    <li><?= $row['owner_name'] ?>, <?= $row['pet_age'] ?> лет, <?= $row['pet_type'] ?>, Вакцинация: <?= $row['has_vaccinations'] ? 'Да' : 'Нет' ?>, Пол: <?= $row['pet_gender ']?></li>
 <?php endforeach; ?>
-
-<?php if(isset($_SESSION['username'])): ?>
-    <p>Данные из сессии:</p>
-    <ul>
-        <li>Имя: <?= $_SESSION['username'] ?></li>
-        <li>Email: <?= $_SESSION['email'] ?></li>
-    </ul>
-<?php else: ?>
-    <p>Данных пока нет.</p>
-<?php endif; ?>
-
-<?php if(isset($_SESSION['errors'])): ?>
-    <ul style="color:red;">
-        <?php foreach($_SESSION['errors'] as $error): ?>
-            <li><?= $error ?></li>
-        <?php endforeach; ?>
-    </ul>
-    <?php unset($_SESSION['errors']); ?>
-<?php endif; ?>
-
-<?php if (isset($_SESSION['api_data'])): ?>
-    <h3>Данные из API:</h3>
-    <pre><?php print_r($_SESSION['api_data']); ?></pre>
-    
-<?php endif; ?>
+</ul>
 
 <a href="form.html">Заполнить форму</a>
-<a href="view.php">Посмотреть все данные</a>
